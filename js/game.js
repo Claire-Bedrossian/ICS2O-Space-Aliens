ce50318804cf0ab4dbb1cc8eacf634607b20d691
Claire-Bedrossian <Claire-Bedrossian@users.noreply.github.com>
2022-11-11 19:06:57 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -12,18 +12,18 @@ const config = {
   width: 1920,
   height: 1080,
   physics: {
-    defult: 'arcade',
+    defult: "arcade",
     arcade: {
-      debug: true
-    }
+      debug: true,
+    },
   },
   // set background colour
   backgroundColor: 0x5f6e7a,
   scale: {
     mode: phaser.Scale.FIT,
     // we place it in the middle of the page.
-    autoCenter: Phaser.Scale.CENTER_BOTH 
-  }
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 };
 
 const game = new Phaser.Game(config);
