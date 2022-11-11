08c7cfcecad46ed215a5ee4b73a1403d09273de4
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-11-11 14:05:00 -0500

    fixed code formating

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* global Phaser */
 
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
+//
+// Created by: Claire Bedrossian
+// Created on: Nov 2022
+// This is Phaser3 configuration file
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    defult: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background colour
   backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH 
+  }
 };
 
 const game = new Phaser.Game(config);
