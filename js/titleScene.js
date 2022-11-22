f7fb35294bddac0c7d8bf000693a9e577da03835
Claire-Bedrossian <Claire-Bedrossian@users.noreply.github.com>
2022-11-22 01:28:51 +0000

    Prettified Code!

--- js/titleScene.js
+++ js/titleScene.js
@@ -9,48 +9,47 @@
 /**
  * Class Is In Title Scene.
  */
- class TitleScene extends Phaser.Scene {
-    /**
-     * This Method Is The Constructor.
-     */
-  
-    constructor() {
-      super({ key: "titleScene" });
-    }
-    /**
-     * Can be defined on your own scenes.
-     * Method called by Scene Manager when scene starts,
-     * before preload() & create().
-     */
-    init(data) {
-      this.cameras.main.setBackgroundColor("#ffffff");
-    }
-    /**
-     * Can be defined on your own scenes.
-     * Use it to load scenes.
-     */
-    preload() {
-      console.log("Title Scene");
-    }
-  
-    /**
-     * Can be defined on your own scenes.
-     * Use it to make your game objects.
-     */
-    create(data) {
-      //pass
-    }
-  
-    /**
-     * Should be overridden by your scenes.
-     * Method gets called once per game step while scene is running.
-     * @param {number} time - The Current Time
-     * @param {number} delta - The delta time in ms since the last frame.
-     */
-    update(time, delta) {
-      //pass
-    }
+class TitleScene extends Phaser.Scene {
+  /**
+   * This Method Is The Constructor.
+   */
+
+  constructor() {
+    super({ key: "titleScene" });
   }
-  
-  export default TitleScene;
-  
+  /**
+   * Can be defined on your own scenes.
+   * Method called by Scene Manager when scene starts,
+   * before preload() & create().
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff");
+  }
+  /**
+   * Can be defined on your own scenes.
+   * Use it to load scenes.
+   */
+  preload() {
+    console.log("Title Scene");
+  }
+
+  /**
+   * Can be defined on your own scenes.
+   * Use it to make your game objects.
+   */
+  create(data) {
+    //pass
+  }
+
+  /**
+   * Should be overridden by your scenes.
+   * Method gets called once per game step while scene is running.
+   * @param {number} time - The Current Time
+   * @param {number} delta - The delta time in ms since the last frame.
+   */
+  update(time, delta) {
+    //pass
+  }
+}
+
+export default TitleScene;
