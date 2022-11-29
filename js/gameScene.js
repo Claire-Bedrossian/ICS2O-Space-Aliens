9e01f1e92953229d1aa9e8b9f484eb95677bcb17
Claire-Bedrossian <Claire-Bedrossian@users.noreply.github.com>
2022-11-29 23:00:09 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -17,8 +17,8 @@ class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: "gameScene" });
 
-    this.background = null
-    this.ship = null
+    this.background = null;
+    this.ship = null;
   }
   /**
    * Can be defined on your own scenes.
@@ -36,8 +36,8 @@ class GameScene extends Phaser.Scene {
     console.log("Game Scene");
 
     //images
-    this.load.image('starBackground', 'assets/starBackground.png')
-    this.load.image('ship', 'assets/spaceShip.png')
+    this.load.image("starBackground", "assets/starBackground.png");
+    this.load.image("ship", "assets/spaceShip.png");
   }
 
   /**
@@ -45,10 +45,10 @@ class GameScene extends Phaser.Scene {
    * Use it to make your game objects.
    */
   create(data) {
-    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
-    this.background.setOrigin(0, 0)
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
+    this.background.setOrigin(0, 0);
 
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
   }
 
   /**
